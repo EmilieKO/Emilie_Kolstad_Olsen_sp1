6d7533c8b466126591151c4d1d607971211ccd5e
Emilie Olsen <krisols96@gmail.com>
2023-04-24 21:31:59 +0200

    First commit

---
+++ wdt_app.js
@@ -0,0 +1,26 @@
+$("document").ready(function () {
+    $("thead").css({
+        "background-color": "#0E8EA8",
+        "font-family": "Consolas",
+        "color": "#fff"
+    })
+    $("table").css({
+        "border": "#212529",
+        "border-radius": "10px",
+        "border-width": "1px",
+        "width": "75%"
+    })
+    $("h1").css({
+        "font-family": "Calibri",
+        "color": "#212529"
+    })
+    $("h2").css({
+        "font-family": "Calibri",
+        "color": "#212529"
+    })
+    $("tbody").css({
+        "font-family": "Calibri",
+        "background-color": "#83D1E1",
+        "color": "#212529"
+    })
+})
